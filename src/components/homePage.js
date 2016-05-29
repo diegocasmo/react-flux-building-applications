7bb3dd6a57b8b1c7221bc9d80593d7fdeea8e4c1
Diego Castillo <diegocasmo@gmail.com>
2016-05-29 12:02:21 -0500

    React Router

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -1,14 +1,17 @@
 'use strict';
 
 var React = require('react');
+var Router = require('react-router');
+var Link = Router.Link;
 
 var HomePage = React.createClass({
 
   render: function() {
     return (
-      <div className="jumbotron">
+      <div className='jumbotron'>
         <h1>Pluralsight Administration</h1>
         <p>React, React Router, and Flux for ultra-responsive web apps.</p>
+        <Link to='about' className='btn btn-primary btn-lg'>Learn more</Link>
       </div>
     );
   }
--- src/main.js
+++ src/main.js
@@ -1,40 +1,8 @@
-$ = jQuery = require('jquery');
+'use strict';
 var React = require('react');
-var Header = require('./components/common/header');
-var HomePage = require('./components/homePage');
-var AboutPage = require('./components/about/aboutPage');
-var AuthorPage = require('./components/authors/authorPage');
+var Router = require('react-router');
+var routes = require('./routes');
 
-(function(win) {
-
-  'use strict';
-
-  var App = React.createClass({
-
-    render: function() {
-      var Child;
-      switch(this.props.route) {
-        case 'about': Child = AboutPage; break;
-        case 'authors': Child = AuthorPage; break;
-        default: Child = HomePage;
-      }
-      return (
-        <div>
-          <Header />
-          <Child />
-        </div>
-      );
-    }
-
-  });
-
-  function render() {
-    var route = win.location.hash.substr(1);
-    React.render(<App route={route} />, document.getElementById('app'));
-  }
-
-  win.addEventListener('hashchange', render);
-  render();
-
-})(window);
-
+Router.run(routes, function(Handler) {
+  React.render(<Handler />, document.getElementById('app'));
+});
